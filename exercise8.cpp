ffef09ef14a87e1aaca5ce2d734a25dc54c290aa
Bryan <65881339+bryanbarceloo@users.noreply.github.com>
2020-09-06 22:36:17 -0700

    Add files via upload

---
+++ exercise8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Input an integer: \n";
+    int = x;
+    cin >> x;
+
+    if (0 == x % 2 ) {
+        std::cout << "The integer " << x << " is even.\n";
+    
+    }
+    
+    else {
+        std::cout << "The integer " << x << " is odd.\n";
+
+    }
+
+    return 0;
+
+}
